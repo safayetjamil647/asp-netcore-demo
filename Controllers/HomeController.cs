687333edff141a9005807e17ef297d0fa8caa39e
safayetjamil647 <work.jamil4@gmail.com>
2021-01-23 01:35:17 +0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = new MockEmployeeRepository();
+        }
+
+        public string Index ()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewData["Employee"] = model;
+            ViewData["PageTitle"]= "Employee Details";
+            return View();
+
+        }
+    }
+}
